48866f4f58e1f148651a654a3f3d8a4c22f93043
Brandon Whaley <redkrieg@gmail.com>
2023-06-15 00:34:39 -0400

    bump version, unify version location, update build tooling

--- make_bundles.sh
+++ make_bundles.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+version_string=$(< VERSION)
+rm -rf build/ firefox.zip chrome.zip # ensure we don't keep deleted files in our packages
 mkdir -p build/
 cp -R _locales icons src build/
 cp manifest.ff.json build/manifest.json
 cd build/
+sed -i "s/VERSION_STRING/${version_string}/" manifest.json
 zip -r ../firefox.zip *
 cd ..
 cp manifest.chrome.json build/manifest.json
 cd build/
+sed -i "s/VERSION_STRING/${version_string}/" manifest.json
 zip -r ../chrome.zip * 
